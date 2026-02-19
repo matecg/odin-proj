dcc04fe9552246643813b7b97f12bec29e65d611
Gody <250132528+matecg@users.noreply.github.com>
2026-02-19 16:23:02 -0300

    Add function to prompt the user for their choice

--- 06-rps/logic.js
+++ 06-rps/logic.js
@@ -1,7 +1,13 @@
 const OPTIONS = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const idx = Math.floor(Math.random() * 3);
     return OPTIONS[idx];
 }
 
+function getHumanChoice() {
+    const choice = prompt("What's your choice? (rock/paper/scissors)", getComputerChoice());
+    return choice.toLowerCase();
+}
